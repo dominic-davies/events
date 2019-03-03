fd825bff3c4106ee3823da5cdb6ed42dbe877ee0
Dominic Davies <domdaviesit@gmail.com>
2019-03-03 16:23:00 +0000

    add event date and description fields to Event model, refactor index page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_03_111437) do
+ActiveRecord::Schema.define(version: 2019_03_03_134829) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2019_03_03_111437) do
     t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "starts_at"
+    t.text "description"
   end
 
 end
