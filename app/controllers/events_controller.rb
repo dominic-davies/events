1b65de02af035e2c0984ee043c60eb007311f205
Dominic Davies <domdaviesit@gmail.com>
2019-03-07 20:15:15 +0000

    Added flash messages for form validation

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -15,9 +15,11 @@ class EventsController < ApplicationController
 
     def update
         @event = Event.find(params[:id])
-        @event.update(event_params)
-        # render "show" ( is get not patch) if reload page will think you are trying to resubmit
-        redirect_to @event
+        if @event.update(event_params)
+            redirect_to @event
+        else
+            render :edit
+        end    
     end
 
     def new
